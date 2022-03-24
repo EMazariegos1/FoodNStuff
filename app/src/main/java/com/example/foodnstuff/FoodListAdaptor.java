a56b7750bdd6e071854d02afd2df157c469726ac
eric.ma5173 <eric.ma5173@vusd.us>
2022-03-24 15:21:25 -0700

    work on the gap

--- app/src/main/java/com/example/foodnstuff/FoodListAdaptor.java
+++ app/src/main/java/com/example/foodnstuff/FoodListAdaptor.java
@@ -5,44 +5,54 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class FoodListAdaptor extends RecyclerView.Adapter {
-    List<Food> foods;
+public class FoodListAdaptor extends RecyclerView.Adapter<FoodListAdaptor.ViewHolder> {
+    List<String> names;
+    List<Integer> images;
     Context context;
     LayoutInflater inflater;
 
-    public FoodListAdaptor(Context c, List<Food> list){
+    public FoodListAdaptor(Context c, List<String> name, List<Integer> image){
         context = c;
-        foods = list;
+        names = name;
+        images = image;
         this.inflater = LayoutInflater.from(c);
     }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = inflater.inflate(R.layout.food_list_item, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.foodImage.setImageResource(images.get(position));
+        holder.foodName.setText(names.get(position));
     }
+
 
     @Override
     public int getItemCount() {
-        return 0;
+        return names.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageButton foodImage;
+        TextView foodName;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            foodImage = itemView.findViewById(R.id.food_image);
+            foodName = itemView.findViewById(R.id.food_name);
         }
     }
 }
