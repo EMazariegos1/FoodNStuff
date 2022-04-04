b81742f8e998ce8dbb6ac39e7d1688f62157c111
eric.ma5173 <eric.ma5173@vusd.us>
2022-04-04 15:13:55 -0700

    test

--- app/src/main/java/com/example/foodnstuff/MainActivity.java
+++ app/src/main/java/com/example/foodnstuff/MainActivity.java
@@ -1,36 +1,16 @@
 package com.example.foodnstuff;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        names = new ArrayList<>();
-        images = new ArrayList<>();
-
-        names.add("one");
-        names.add("two");
-        names.add("three");
-        names.add("four");
-        names.add("four");
-        names.add("four");
-        names.add("four");
-        names.add("four");
-        images.add(R.drawable.chicken);
-        images.add(R.drawable.chicken);
-        images.add(R.drawable.chicken);
-        images.add(R.drawable.chicken);
-        images.add(R.drawable.chicken);
-        images.add(R.drawable.chicken);
-        images.add(R.drawable.chicken);
-        images.add(R.drawable.chicken);
-//code for recyclerview and see if this works
-        adaptor = new FoodListAdaptor(this, names, images);
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL,false);
-        foodList.setLayoutManager(gridLayoutManager);
-        foodList.setAdapter(adaptor);
     }
-
 }
